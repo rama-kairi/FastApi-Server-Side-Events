0dfe4f31a1ff6343cbc417d50dfdc5f81ebd1899
Ramananda Kairi <ramananda.kairi@gmail.ccom>
2022-02-22 13:26:16 +0530

    Server Side Events Example

---
+++ server/program.py
@@ -0,0 +1,26 @@
+"""
+program.py
+This script will keep adding logs to our logger file.
+"""
+
+import logging
+import os
+import time
+
+# create logger with log app
+real_path = os.path.realpath(__file__)
+dir_path = os.path.dirname(real_path)
+LOGFILE = f"{dir_path}/test.log"
+logger = logging.getLogger('log_app')
+logger.setLevel(logging.DEBUG)
+fh = logging.FileHandler(LOGFILE)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+fh.setFormatter(formatter)
+logger.addHandler(fh)
+
+#infinite while loop printing to our log file.
+i = 0
+while True:
+    logger.info(f"log message num: {i}")
+    i += 1
+    time.sleep(0.3)
